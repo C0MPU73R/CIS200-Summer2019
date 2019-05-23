2d2c9cb0ec8c060001325ed3b8ddffadf8fa3165
Eric Charnesky <eric.charnesky@careevolution.com>
2019-05-23 11:47:46 -0400

    move powerball

--- Math/Math/MathFunctions.h
+++ Math/Math/MathFunctions.h
@@ -10,3 +10,23 @@ public:
 	double getCircumference();
 	double getRadius();
 };
+
+Circle::Circle(double radius)
+{
+	this->radius = radius;
+}
+
+double Circle::getArea()
+{
+	return radius * radius * PI;
+}
+
+double Circle::getCircumference()
+{
+	return radius * 2 * PI;
+}
+
+double Circle::getRadius()
+{
+	return radius;
+}
--- PowerBall/PowerBall/PowerBallTicket.h
+++ PowerBall/PowerBall/PowerBallTicket.h
@@ -10,3 +10,28 @@ public:
 	PowerBallTicket(int white1, int white2, int white3, int white4, int white5, int red);
 	int getWinnings(PowerBallTicket& winningTicket);
 };
+
+PowerBallTicket::PowerBallTicket()
+{
+	whiteNumbers[0] = 0;
+	whiteNumbers[1] = 0;
+	whiteNumbers[2] = 0;
+	whiteNumbers[3] = 0;
+	whiteNumbers[4] = 0;
+	redNumber = 0;
+}
+
+PowerBallTicket::PowerBallTicket(int white1, int white2, int white3, int white4, int white5, int red)
+{
+	whiteNumbers[0] = 0;
+	whiteNumbers[1] = 0;
+	whiteNumbers[2] = 0;
+	whiteNumbers[3] = 0;
+	whiteNumbers[4] = 0;
+	redNumber = 0;
+}
+
+int PowerBallTicket::getWinnings(PowerBallTicket & winningTicket)
+{
+	return 0;
+}
