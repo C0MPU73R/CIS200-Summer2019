16f01223a642d2a83e1ae4539e2d002cfd6963df
Eric Charnesky <eric.charnesky@careevolution.com>
2019-05-30 00:00:48 -0400

    PowerBall solution

--- PowerBall/UnitTest1/unittest1.cpp
+++ PowerBall/UnitTest1/unittest1.cpp
@@ -41,7 +41,136 @@ namespace UnitTest1
 
 			// Assert
 			Assert::AreEqual(expectedWinnings, actualWinnings);
+		}
 
+		TEST_METHOD(PowerBallFourWhiteAndRedMatch)
+		{
+			// AAA
+
+			// Arrange
+			int expectedWinnings = 50000;
+			PowerBallTicket winningTicket(1, 2, 3, 4, 7, 6);
+			PowerBallTicket myTicket(5, 4, 3, 2, 1, 6);
+
+			// Act
+			int actualWinnings = myTicket.getWinnings(winningTicket);
+
+			// Assert
+			Assert::AreEqual(expectedWinnings, actualWinnings);
+		}
+
+		TEST_METHOD(PowerBallFourWhiteMatch)
+		{
+			// AAA
+
+			// Arrange
+			int expectedWinnings = 100;
+			PowerBallTicket winningTicket(1, 2, 3, 4, 7, 7);
+			PowerBallTicket myTicket(5, 4, 3, 2, 1, 6);
+
+			// Act
+			int actualWinnings = myTicket.getWinnings(winningTicket);
+
+			// Assert
+			Assert::AreEqual(expectedWinnings, actualWinnings);
+		}
+
+		TEST_METHOD(PowerBallThreeWhiteAndRedMatch)
+		{
+			// AAA
+
+			// Arrange
+			int expectedWinnings = 100;
+			PowerBallTicket winningTicket(1, 2, 3, 6, 7, 7);
+			PowerBallTicket myTicket(5, 4, 3, 2, 1, 7);
+
+			// Act
+			int actualWinnings = myTicket.getWinnings(winningTicket);
+
+			// Assert
+			Assert::AreEqual(expectedWinnings, actualWinnings);
+		}
+
+		TEST_METHOD(PowerBallThreeWhiteMatch)
+		{
+			// AAA
+
+			// Arrange
+			int expectedWinnings = 7;
+			PowerBallTicket winningTicket(1, 2, 3, 6, 7, 8);
+			PowerBallTicket myTicket(5, 4, 3, 2, 1, 7);
+
+			// Act
+			int actualWinnings = myTicket.getWinnings(winningTicket);
+
+			// Assert
+			Assert::AreEqual(expectedWinnings, actualWinnings);
+		}
+
+		TEST_METHOD(PowerBallTwoWhiteAndRedMatch)
+		{
+			// AAA
+
+			// Arrange
+			int expectedWinnings = 7;
+			PowerBallTicket winningTicket(1, 2, 9, 6, 7, 8);
+			PowerBallTicket myTicket(5, 4, 3, 2, 1, 8);
+
+			// Act
+			int actualWinnings = myTicket.getWinnings(winningTicket);
+
+			// Assert
+			Assert::AreEqual(expectedWinnings, actualWinnings);
+		}
+
+		TEST_METHOD(PowerBallRedMatch)
+		{
+			// AAA
+
+			// Arrange
+			int expectedWinnings = 4;
+			PowerBallTicket winningTicket(10, 8, 9, 6, 7, 8);
+			PowerBallTicket myTicket(5, 4, 3, 2, 1, 8);
+
+			// Act
+			int actualWinnings = myTicket.getWinnings(winningTicket);
+
+			// Assert
+			Assert::AreEqual(expectedWinnings, actualWinnings);
+		}
+
+		TEST_METHOD(PowerBallNoMatch)
+		{
+			// AAA
+
+			// Arrange
+			int expectedWinnings = 0;
+			PowerBallTicket winningTicket(10, 8, 9, 6, 7, 9);
+			PowerBallTicket myTicket(5, 4, 3, 2, 1, 8);
+
+			// Act
+			int actualWinnings = myTicket.getWinnings(winningTicket);
+
+			// Assert
+			Assert::AreEqual(expectedWinnings, actualWinnings);
+		}
+
+		TEST_METHOD(PowerBallDuplicateWhiteNumbers)
+		{
+			// AAA
+
+			// Arrange
+
+			// Act
+			try 
+			{
+				PowerBallTicket myTicket(1, 4, 3, 2, 1, 7);
+				Assert::Fail();
+			}
+			catch (invalid_argument &e)
+			{
+
+			}
 		}
 
 	};
